94705339aaf6867e5537461536b21051d1dc315a
SivakumarRevuri <sivakumarr96@gmail.com>
2019-05-07 18:28:40 +0530

    hibernate basics

---
+++ src/app/Basics.java
@@ -0,0 +1,41 @@
+package app;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import model.Employee;
+
+public class Basics {
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration().configure("resources/hibernate.cfg.xml").buildSessionFactory();
+		Session session = factory.openSession();
+	
+		// fetch single records
+		new Basics().fetchRecords(session);
+		// to fetch multiple records
+		new Basics().fetchMultipleRecords(session);
+	}
+	
+	private void fetchMultipleRecords(Session session) {
+		String sql = "select empId,ename,job,salary from Employee";
+		@SuppressWarnings("unchecked")
+		Query<Object> query = session.createQuery(sql);
+		List<Object> list = query.list();
+		
+		for (Object object : list) {
+			Object[] array = (Object[]) object;
+			for (Object values : array) {
+				System.out.println(values);
+			}
+		}
+		
+	}
+
+	private void fetchRecords(Session session) {
+		System.out.println(session.get(Employee.class, 1L));
+	}
+}
---
+++ src/model/Employee.java
@@ -0,0 +1,66 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+	@Id
+	private long empId;
+	private String ename;
+	private String job;
+	private double salary;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(long empId, String ename, String job, double salary) {
+		super();
+		this.empId = empId;
+		this.ename = ename;
+		this.job = job;
+		this.salary = salary;
+	}
+
+	public long getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(long empId) {
+		this.empId = empId;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", ename=" + ename + ", job=" + job + ", salary=" + salary + "]";
+	}
+
+}
